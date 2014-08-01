f218e408cf362c00b367dfffb11cf04429931c98
Bevan Arps <unrepentantgeek@outlook.com>
2014-08-01 19:09:20 +1200

    Demo project.

---
+++ src/Niche.CommandLine.Demo/Driver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niche.CommandLine.Demo
+{
+    /// <summary>
+    /// Command line interface for the demo
+    /// </summary>
+    public class Driver
+    {
+        public bool IsVerbose { get; private set; }
+
+        public IList<string> SearchTerms { get { return mSearchTerms; } }
+
+        [Description("Verbose output")]
+        public void Verbose()
+        {
+            IsVerbose = true;
+        }
+
+        [Description("Find items by keyword")]
+        public void Find(string search)
+        {
+            mSearchTerms.Add(search);
+        }
+
+        private readonly List<string> mSearchTerms = new List<string>();
+    }
+}
---
+++ src/Niche.CommandLine.Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niche.CommandLine;
+
+namespace Niche.CommandLine.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new CommandLineProcessor(args);
+            var driver = new Driver();
+            var files = processor.Configure(driver);
+
+
+        }
+    }
+}
