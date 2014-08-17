d11b04da346a54daa05101a3339e2441309369c0
Bevan Arps <unrepentantgeek@outlook.com>
2014-08-17 15:24:35 +1200

    New attribute [Required]

---
+++ src/Niche.CommandLine/RequiredAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niche.CommandLine
+{
+    /// <summary>
+    /// Mark a commandline parameter as mandatory
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class RequiredAttribute : Attribute
+    {
+        // Nothing
+    }
+}
