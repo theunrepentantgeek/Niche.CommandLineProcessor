73ebc7de42193a54b82a0189cae6e4116333d41e
Bevan Arps <unrepentantgeek@outlook.com>
2014-08-19 21:24:54 +1200

    Extension method string.As<T> for conversion.

---
+++ src/Niche.CommandLine.Tests/StringExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niche.CommandLine.Tests
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void AsString_givenString_returnString()
+        {
+            var sample = "sample";
+            var result = sample.As<string>();
+            Assert.That(result, Is.EqualTo(sample));
+        }
+
+        [Test]
+        public void AsInt_givenNumber_returnInt()
+        {
+            var sample = "42";
+            var result = sample.As<int>();
+            Assert.That(result, Is.EqualTo(42));
+        }
+
+        [Test]
+        public void AsColor_givenColor_returnsColor()
+        {
+            var sample = "Red";
+            var result = sample.As<Color>();
+            Assert.That(result, Is.EqualTo(Color.Red));
+        }
+
+        [Test]
+        public void AsVersion_givenVersion_returnsVersion()
+        {
+            var sample = "1.2.3.4";
+            var result = sample.As<Version>();
+            Assert.That(result.ToString(4), Is.EqualTo("1.2.3.4"));
+        }
+    }
+}
---
+++ src/Niche.CommandLine/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niche.CommandLine
+{
+    /// <summary>
+    /// Utility conversion extensions to String
+    /// </summary>
+    internal static class StringExtensions
+    {
+        public static T As<T>(this string value)
+        {
+            object v = value;
+
+            // If a string, just return it
+            if (typeof(T) == typeof(string))
+            {
+                return (T)v;
+            }
+
+            // Use a TypeConverter if one is available
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            if (converter != null && converter.CanConvertFrom(typeof(string)))
+            {
+                return (T)converter.ConvertFromString(value);
+            }
+
+            // Look for a constructor that takes a string
+            var constructor = typeof(T).GetConstructor(new[] { typeof(string) });
+            if (constructor != null)
+            {
+                return (T)constructor.Invoke(new[] { value });
+            }
+
+            var message = string.Format("Cannot convert {0} into {1}", value, typeof(T).Name);
+            throw new InvalidOperationException(message);
+        }
+    }
+}
