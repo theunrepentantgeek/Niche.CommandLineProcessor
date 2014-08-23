15547c34e7b947243eb7a1be3f150734af9f55b3
Bevan Arps <unrepentantgeek@outlook.com>
2014-08-23 12:26:14 +1200

    Tests for FindDescription()

--- src/Niche.CommandLine.Tests/CommandLineOptionBaseTests.cs
+++ src/Niche.CommandLine.Tests/CommandLineOptionBaseTests.cs
@@ -10,6 +10,21 @@ namespace Niche.CommandLine.Tests
     [TestFixture]
     public class CommandLineOptionBaseTests
     {
-        
+        [Test]
+        public void FindDescription_givenMemberWithDescription_returnsDescription()
+        {
+            var method = typeof(SampleDriver).GetMethod("Help");
+            var description = CommandLineOptionBase.FindDescription(method);
+            Assert.That(description, Is.EqualTo("Show Help"));
+        }
+
+        [Test]
+        public void FindDescription_givenMemberWithNoDescription_returnsEmptyString()
+        {
+            var method = typeof(SampleDriver).GetMethod("Verbose");
+            var description = CommandLineOptionBase.FindDescription(method);
+            Assert.That(description, Is.EqualTo(string.Empty));
+        }
+
     }
 }
