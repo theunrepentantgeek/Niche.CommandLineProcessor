44a88f0c5623029c137db0cc51899cebfcc89bbe
Bevan Arps <unrepentantgeek@outlook.com>
2014-10-31 21:33:55 +1300

    Command line mode tests and fixes.

---
+++ src/Niche.CommandLine.Tests/CommandLineModeTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niche.CommandLine.Tests
+{
+    [TestFixture]
+    public class CommandLineModeTests
+    {
+        [Test]
+        public void Constructor_givenNullInstance_throwsException()
+        {
+            var method = typeof(BaseDriver).GetMethod("Help");
+            Assert.Throws<ArgumentNullException>(
+                () => new CommandLineMode<BaseDriver>(null, method));
+        }
+
+        [Test]
+        public void Constructor_givenNullMethod_throwsException()
+        {
+            var driver = new BaseDriver();
+            Assert.Throws<ArgumentNullException>(
+                () => new CommandLineMode<BaseDriver>(driver, null));
+        }
+
+        [Test]
+        public void Constructor_forHelp_initialisesName()
+        {
+            // Arrange
+            var driver = new BaseDriver();
+            var method = typeof(BaseDriver).GetMethod("Help");
+            // Act
+            var mode = new CommandLineMode<BaseDriver>(driver, method);
+            // Assert
+            Assert.That(mode.Name, Is.EqualTo("help"));
+        }
+
+        [Test]
+        public void Constructor_forTestPerformance_initialisesName()
+        {
+            var driver = new BaseDriver();
+            var method = typeof(BaseDriver).GetMethod("TestPerformance");
+            var mode = new CommandLineMode<BaseDriver>(driver, method);
+            Assert.That(mode.Name, Is.EqualTo("test-performance"));
+        }
+
+        [Test]
+        public void Constructor_forHelp_initialisesDescription()
+        {
+            // Arrange
+            var driver = new BaseDriver();
+            var method = typeof(BaseDriver).GetMethod("Help");
+            // Act
+            var mode = new CommandLineMode<BaseDriver>(driver, method);
+            // Assert
+            Assert.That(mode.Description, Is.EqualTo("Show Help"));
+        }
+
+        [Test]
+        public void Activate_forTestPerformance_callsMethod()
+        {
+            var driver = new BaseDriver();
+            var method = typeof(BaseDriver).GetMethod("TestPerformance");
+            var mode = new CommandLineMode<BaseDriver>(driver, method);
+            var result = mode.Activate();
+            Assert.That(result, Is.InstanceOf<TestDriver>());
+        }
+    }
+}
---
+++ src/Niche.CommandLine/CommandLineMode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niche.CommandLine
+{
+    public class CommandLineMode<T>
+    {
+        /// <summary>
+        /// Gets the name of this mode
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets or sets a description of this option
+        /// </summary>
+        public string Description { get; private set; }
+
+
+        public CommandLineMode(T instance, MethodInfo method)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            if (method == null)
+            {
+                throw new ArgumentNullException("method");
+            }
+
+            if (!method.DeclaringType.IsAssignableFrom(instance.GetType()))
+            {
+                throw new ArgumentException("Expect method to be callable on instance");
+            }
+
+            mInstance = instance;
+            mMethod = method;
+            Description = CommandLineOptionBase.FindDescription(method);
+
+            Name = CamelCase.ToDashedName(method.Name);
+        }
+
+        /// <summary>
+        /// Activate this switch when found
+        /// </summary>
+        public T Activate()
+        {
+            return (T)mMethod.Invoke(mInstance, null);
+        }
+
+        private readonly object mInstance;
+        private readonly MethodInfo mMethod;
+    }
+}
