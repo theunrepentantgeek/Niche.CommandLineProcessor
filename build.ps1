39a4f7fc86d1f925b51c7f04ad7e0943f46535f0
Bevan Arps <bevan.arps@outlook.com>
2017-08-11 16:42:50 +1200

    Version each build according to branch

--- build.ps1
+++ build.ps1
@@ -5,14 +5,45 @@
 properties {
     $baseDir = resolve-path .\
     $buildDir = "$baseDir\build"
+    $srcDir = resolve-path $baseDir\src
 }
 
-Task Integration.Build -Depends Debug.Build, Compile.Assembly, Unit.Tests
+Task Integration.Build -Depends Generate.VersionInfo, Debug.Build, Compile.Assembly, Unit.Tests
 
-Task Formal.Build -Depends Release.Build, Compile.Assembly, Unit.Tests
+Task Formal.Build -Depends Release.Build, Generate.Version, Compile.Assembly, Compile.NuGet, Unit.Tests
 
-Task CI.Build -Depends Debug.Build, Compile.Assembly, Unit.Tests
+Task CI.Build -Depends Debug.Build, Generate.Version, Compile.Assembly, Unit.Tests
 
+
+Task Compile.Assembly -Depends Requires.BuildType, Requires.MSBuild, Requires.BuildDir, Generate.VersionInfo {
+
+    exec { 
+        & $msbuildExe /p:Configuration=$buildType /verbosity:minimal /fileLogger /flp:verbosity=detailed`;logfile=$buildDir\Niche.CommandLine.txt .\Niche.CommandLine.sln 
+    }
+}  
+
+Task Compile.NuGet -Depends Requires.NuGet, Requires.BuildType, Compile.Assembly {
+
+    exec {
+        & $nugetExe pack $srcDir\Niche.CommandLine\Niche.CommandLine.nuspec -version 2.1.0.0 -outputdirectory $buildDir -basePath $buildDir\Niche.CommandLine\$buildType
+    }
+}
+
+Task Unit.Tests -Depends Requires.XUnitConsole, Compile.Assembly {
+
+    exec {
+        & $xunitExe .\build\Niche.CommandLine.Tests\Debug\Niche.CommandLine.Tests.dll
+    }
+}
+
+Task Coverage.Tests -Depends Requires.OpenCover, Compile.Assembly {
+
+}
+
+## ----------------------------------------------------------------------------------------------------
+##   Specifiers 
+## ----------------------------------------------------------------------------------------------------
+## Tasks used to configure the way the build will execute
 
 Task Release.Build {
     $script:buildType = "Release"
@@ -24,19 +55,84 @@ Task Debug.Build {
     Write-Host "Debug build configured"
 }
 
-Task Compile.Assembly -Depends Requires.BuildType, Requires.MSBuild, Requires.BuildDir {
 
-    exec { 
-        & $msbuildExe /p:Configuration=$buildType /verbosity:minimal /fileLogger /flp:verbosity=detailed`;logfile=$buildDir\Niche.CommandLine.txt .\Niche.CommandLine.sln 
+
+## ----------------------------------------------------------------------------------------------------
+##   Generate 
+## ----------------------------------------------------------------------------------------------------
+## Tasks for creating source files and other useful things
+
+# Generate the version number to use for this build
+Task Generate.Version {
+
+    $script:version = get-content $baseDir\version.txt -ErrorAction SilentlyContinue
+    if ($version -eq $null) {
+        throw "Unable to load .\version.txt"
     }
-}  
+    Write-Host "Version          $script:version"
 
-Task Unit.Tests -Depends Requires.XUnitConsole, Compile.Assembly {
+    $versionLastUpdated = git rev-list -1 HEAD $baseDir\version.txt
+    $script:patchVersion = git rev-list "$versionLastUpdated..HEAD" --count
+    Write-Host "Patch            $patchVersion"
 
-    exec {
-        & $xunitExe .\build\Niche.CommandLine.Tests\Debug\Niche.CommandLine.Tests.dll
+    $branch = git name-rev --name-only HEAD
+    Write-Host "Current Branch   $branch"
+
+    $commit = git rev-parse --short head
+    Write-Host "Current Commit   $commit"
+
+
+    if ($branch -eq "master") {
+        $script:semanticVersion = "$version.$patchVersion"
+    }
+    elseif ($branch -eq "develop") {
+        $script:semanticVersion = "$version.$patchVersion-beta.$commit"
+    }
+    else {
+        $semanticBranch = $branch -replace "[^A-Za-z0-9-]+", "."
+        $script:semanticVersion = "$version.$patchVersion-alpha.$semanticBranch.$commit"
+    }
+
+    Write-Host "Semantic version $semanticVersion"
+}
+
+# Generate a VersionInfo.cs file for this build
+Task Generate.VersionInfo -Depends Generate.Version {
+
+    foreach($assemblyInfo in (get-childitem $srcDir\AssemblyInfo.cs -recurse)) {
+        $versionInfo = Join-Path $assemblyInfo.Directory "VersionInfo.cs"
+        set-content $versionInfo "// Generated file - do not modify",
+            "using System.Reflection;",
+            "[assembly: AssemblyVersion(`"$version`")]",
+            "[assembly: AssemblyFileVersion(`"$version.$patchVersion`")]",
+            "[assembly: AssemblyInformationalVersion(`"$semanticVersion`")]"
+        Write-Host "Generated $versionInfo"
     }
 }
+
+## ----------------------------------------------------------------------------------------------------
+##   Configure 
+## ----------------------------------------------------------------------------------------------------
+## Tasks for finding or creating folders that are needed
+
+Task Configure.xUnitResultFolder {
+
+    $script:xUnitResultFolder = join-path $buildDir xUnit.results
+    if (test-path $xUnitResultFolder) {
+        Write-Host "Found XUnit results folder: $xUnitResultFolder"
+    }
+    else {
+        mkdir $xUnitResultFolder -erroraction silentlycontinue | Out-Null
+        Write-Host "Created XUnit results folder: $xUnitResultFolder"
+    }
+}
+
+
+## ----------------------------------------------------------------------------------------------------
+##   Requires 
+## ----------------------------------------------------------------------------------------------------
+## Tasks for finding required resources that must already be available.
+## This includes executables and other tools
 
 Task Requires.MSBuild {
 
@@ -67,8 +163,7 @@ Task Requires.XUnitConsole {
     $script:xunitExe =
         resolve-path ".\packages\xunit.runner.console.*\tools\xunit.console.exe"
 
-    if ($xunitExe -eq $null)
-    {
+    if ($xunitExe -eq $null) {
         throw "Failed to find XUnit.Console.exe"
     }
 
@@ -83,8 +178,37 @@ Task Requires.BuildType {
     }
 
     Write-Host "$buildType build confirmed"
-    
 }
+
+Task Requires.NuGet { 
+
+    $script:nugetExe =
+        resolve-path ".\packages\NuGet.CommandLine.*\tools\nuget.exe"
+
+    if ($nugetExe -eq $null)
+    {
+        throw "Failed to find nuget.exe"
+    }
+
+    Write-Host "Found Nuget here: $nugetExe"
+}
+
+Task Requires.OpenCover { 
+
+    $script:opencoverExe =
+        resolve-path ".\packages\OpenCover.*\tools\OpenCover.Console.exe"
+
+    if ($opencoverExe -eq $null)
+    {
+        throw "Failed to find opencover.console.exe"
+    }
+
+    Write-Host "Found OpenCover here: $opencoverExe"
+}
+
+## ----------------------------------------------------------------------------------------------------
+##   Utility Methods
+## ----------------------------------------------------------------------------------------------------
 
 formatTaskName { 
 	param($taskName) 
