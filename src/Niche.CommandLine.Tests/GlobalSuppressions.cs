9bc24977a76d1f159584242e50d60c1af42228c1
Bevan Arps <bevan.arps@outlook.com>
2017-09-16 22:09:32 +1200

    Suppress code analysis messages

---
+++ src/Niche.CommandLine.Tests/GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Design",
+    "CA1052:Static holder types should be Static or NotInheritable",
+    Justification = "This project uses wrapper classes to group tests.")]
+[assembly: SuppressMessage(
+    "Design",
+    "RCS1102:Mark class as static.",
+    Justification = "This project uses wrapper classes to group tests.")]
+
---
+++ src/Niche.ConsoleLogging/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Performance",
+    "RCS1096:Use bitwise operation instead of calling 'HasFlag'.",
+    Justification = "This project prefers the clarity of HasFlag()")]
