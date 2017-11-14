0c23731d22d6406963ca9301d36813d690d6504e
Bevan Arps <bearps@microsoft.com>
2017-11-14 15:07:04 +1300

    Return -1 to caller if psake build fails

--- formal-build.ps1
+++ formal-build.ps1
@@ -2,3 +2,6 @@
 dotnet restore
 . .\scripts\bootstrap.ps1
 invoke-psake ./build.ps1 -Task Formal.Build
+if ($psake.build_success -eq $false) {
+    return -1
+}
--- integration-build.ps1
+++ integration-build.ps1
@@ -1,3 +1,6 @@
 dotnet restore
 . .\scripts\bootstrap.ps1
 invoke-psake ./build.ps1 -Task Integration.Build
+if ($psake.build_success -eq $false) {
+    return -1
+}
